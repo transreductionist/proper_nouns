0abf46cf008af70d92cf42c1c325126f2916fb54
transreductionist <transreductionist@gmail.com>
2019-07-08 00:15:11 -0400

    Add files.

---
+++ funcs/build_counts.py
@@ -0,0 +1,42 @@
+"""Build counts for names and words."""
+
+from proper_noun_original.funcs.utilities import dump_data_to_pickle
+from proper_noun_original.funcs.utilities import FIRST_NAMES_PATHS
+from proper_noun_original.funcs.utilities import get_counts
+from proper_noun_original.funcs.utilities import load_pickle
+from proper_noun_original.funcs.utilities import normalize_counts
+from proper_noun_original.funcs.utilities import SURNAMES_PATHS
+from proper_noun_original.funcs.utilities import WORDS_PATHS
+
+
+def build_first_name_counts():
+    """Build first name counts."""
+
+    print('\n***** Build first name counts.')
+    first_names = load_pickle(FIRST_NAMES_PATHS['pkl'])
+    counts_first = get_counts(first_names)
+    counts_first_normalized = normalize_counts(counts_first)
+    dump_data_to_pickle(FIRST_NAMES_PATHS['pkl_cnts'], counts_first)
+    dump_data_to_pickle(FIRST_NAMES_PATHS['pkl_cnts_norm'], counts_first_normalized)
+
+
+def build_surname_counts():
+    """Build surname counts."""
+
+    print('***** Build surname counts.')
+    surnames = load_pickle(SURNAMES_PATHS['pkl'])
+    counts_surname = get_counts(surnames)
+    counts_surname_normalized = normalize_counts(counts_surname)
+    dump_data_to_pickle(SURNAMES_PATHS['pkl_cnts'], counts_surname)
+    dump_data_to_pickle(SURNAMES_PATHS['pkl_cnts_norm'], counts_surname_normalized)
+
+
+def build_word_counts():
+    """Build word counts."""
+
+    print('***** Build word counts.\n')
+    words = load_pickle(WORDS_PATHS['pkl'])
+    counts_word = get_counts(words)
+    counts_word_normalized = normalize_counts(counts_word)
+    dump_data_to_pickle(WORDS_PATHS['pkl_cnts'], counts_word)
+    dump_data_to_pickle(WORDS_PATHS['pkl_cnts_norm'], counts_word_normalized)
---
+++ scripts/create_pickle_data.py
@@ -0,0 +1,10 @@
+"""Build pickle data from raw resources."""
+
+from proper_noun_original.funcs.build_pickles import build_first_name_data
+from proper_noun_original.funcs.build_pickles import build_surname_data
+from proper_noun_original.funcs.build_pickles import build_word_data
+
+
+build_first_name_data()
+build_surname_data()
+build_word_data()
